6a823423b6e43b061816742a4950bd5cefdad38c
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2023-07-26 12:06:18 +0000

    commented messages component

--- src/components/message/index.js
+++ src/components/message/index.js
@@ -1,28 +1,34 @@
+// import necessary modules and components
 import { View, Text, StyleSheet, StatusBar } from "react-native";
 import React from "react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-dayjs.extend(relativeTime);
+dayjs.extend(relativeTime); // Extend dayjs with the relativeTime plugin
 
 const Message = ({ message }) => {
+  // Function to check if the message belongs to the current user
   const isMyMessage = () => {
-    return message.user.id === "u1";
+    return message.user.id === "u1"; // Assuming the current user's id is "u1"
   };
+
+  // Function to check if the message is the first one in the chat
   const isFirstMessage = () => {
-    return message.id === "m13";
+    return message.id === "m13"; // Assuming the first message's id is "m13"
   };
+
   return (
     <View
       style={[
         styles.container,
         {
-          backgroundColor: isMyMessage() ? "#e2f7c2" : "white",
-          alignSelf: isMyMessage() ? "flex-end" : "flex-start",
-          marginTop: isFirstMessage() ? StatusBar.currentHeight : null,
+          backgroundColor: isMyMessage() ? "#F0FDDF" : "white", // Set background color based on whether it's the user's message or not
+          alignSelf: isMyMessage() ? "flex-end" : "flex-start", // Align the message to the right if it's the user's message, otherwise align to the left
+          marginTop: isFirstMessage() ? StatusBar.currentHeight : null, // Add marginTop to the first message to accommodate the status bar height
         },
       ]}
     >
-      <Text style={styles.txt}>{message.text}</Text>
+      {/* display the message text and time */}
+      <Text style={styles.txt}>{message.text}</Text> 
       <Text style={styles.time}>{dayjs(message.createdAt).fromNow(true)}</Text>
     </View>
   );
@@ -30,12 +36,9 @@ const Message = ({ message }) => {
 
 const styles = StyleSheet.create({
   container: {
-    // backgroundColor: "white",
-    // alignSelf: "flex-start",
-    // marginTop: StatusBar.currentHeight,
     margin: 5,
     padding: 10,
-    borderRadius: 10,
+    borderRadius: 15,
     maxWidth: "80%",
     shadowColor: "#000000",
     shadowOffset: {
@@ -44,7 +47,7 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.16,
     shadowRadius: 1.51,
-    elevation: 2,
+    elevation: 4,
   },
   time: {
     color: "gray",
