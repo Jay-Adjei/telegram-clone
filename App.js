ee703fafe2c06447b864003ea35a5e0af5bf373a
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2023-07-26 10:50:38 +0000

    commented app.js

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
+// import necessary components
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeStack from "./src/Navigation";
 
+// Define the main component of the app
 const App = () => {
   return (
+    // Wrapping the main component with NavigationContainer to enable navigation
     <NavigationContainer style={styles.container}>
       <StatusBar style="light" />
       <HomeStack />
     </NavigationContainer>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
