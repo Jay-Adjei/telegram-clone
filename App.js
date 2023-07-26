a22c292d3147368004131850ba94bac80cd3426d
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2023-07-26 11:00:59 +0000

    commented app.js

--- App.js
+++ App.js
@@ -1,27 +1,31 @@
 // import necessary components
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import HomeStack from "./src/Navigation";
+import { StatusBar } from "expo-status-bar"; //importing status bar from expo
+import { StyleSheet } from "react-native"; // importing necessary components from react-native
+import React from "react"; //importing react
+import { NavigationContainer } from "@react-navigation/native"; //importing navigation container from react navigation
+import HomeStack from "./src/Navigation"; //importing the home stack from the navigation folder
 
 // Define the main component of the app
 const App = () => {
   return (
     // Wrapping the main component with NavigationContainer to enable navigation
     <NavigationContainer style={styles.container}>
+      {/* Setting the status bar to be white */}
       <StatusBar style="light" />
+      {/* Rendering the HomeStack component from navigation */}
       <HomeStack />
     </NavigationContainer>
   );
 };
 
+// making styles for the app
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    flex: 1, // The container will take up all available space
+    alignItems: "center", // Content will be aligned horizontally in the center
+    justifyContent: "center", // Content will be aligned vertically in the center
   },
 });
 
+// Export the main component as the default export of the module
 export default App;
